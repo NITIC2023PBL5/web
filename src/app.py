0fc2d8502bdd6007ef7e1cc786fc3508ed8b3c60
yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2023-06-09 16:54:18 +0900

    🚚 move app.py

--- src/app.py
+++ src/app.py
@@ -13,5 +13,9 @@ def hello_world():
     return "Hello, World!"
 
 
+def run():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port=port)
+
 if __name__ == "__main__":
-    app.run(port=PORT)
+    run()
