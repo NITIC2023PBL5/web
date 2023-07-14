7e34925ca67214231fd43b097bf2d96a8b515879
yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2023-07-14 16:02:06 +0900

    🩹 fix import

--- src/routes/notify.py
+++ src/routes/notify.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, redirect, request
 
-from ..services.lineNotify import auth as lineNotifyAuth
+from ..services.lineNotify.auth import (
+    get_redirect_auth_url,
+    check_state,
+    get_access_token,
+)
 
 notify_app = Blueprint("notify", __name__)
 
@@ -8,7 +12,7 @@ notify_app = Blueprint("notify", __name__)
 @notify_app.route("/auth")
 def auth():
     # TODO: ユーザーIDをセッションから取得する
-    url = lineNotifyAuth.get_redirect_auth_url("USER_ID")
+    url = get_redirect_auth_url("USER_ID")
     return redirect(url)
 
 
@@ -18,8 +22,8 @@ def callback():
     state = request.form["state"]
 
     # TODO: ユーザーIDをセッションから取得する
-    if not lineNotifyAuth.check_state(state, "USER_ID"):
+    if not check_state(state, "USER_ID"):
         return redirect("/", code=400)
 
-    access_token = lineNotifyAuth.get_access_token(code)
+    access_token = get_access_token(code)
     return access_token
